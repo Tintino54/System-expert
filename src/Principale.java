825f47ad60f2426d10781aa863c39364e9848cdc
Valentin <valentin.ginisty@gmail.com>
2015-10-02 18:48:17 +0200

    fin première version

--- src/Principale.java
+++ src/Principale.java
@@ -8,12 +8,12 @@ public class Principale {
 		ArrayList<String> BF = new ArrayList<String>();
 		//ce que nous recherchons
 		ArrayList<String> objectif = new ArrayList<String>();
-		
+
 		//BF = {A,B,C}
 		BF.add("A");
 		BF.add("B");
 		BF.add("C");
-			
+
 		//R1 = A->B
 		//R2 = ...
 		regles.add(new Regle("A->B"));
@@ -21,16 +21,47 @@ public class Principale {
 		regles.add(new Regle("C^F->A"));
 		regles.add(new Regle("D->F"));
 		regles.add(new Regle("D^A->Z"));
-		
-		
+
+
 		//objectif : {F}
 		objectif.add("F");
-		//affiche les règles
-		for(int i = 0; i < regles.size(); i++){
-			System.out.println(regles.get(i).getPrem()+" "+regles.get(i).getRes());
-		}
+
+		// On fait un chaînage avant, donc on cherche à aggrandir notre BF jusqu'à ce qu'il contienne
+		//la ou les solutions recherchées
+
+		//tant qu'on a pas trouvé l'objectif dans notre ensemble de faits
+		//ou lorsque l'on fait un tour de boucle sans effectuer aucune opération
+
+		//arret permet d'arrêter la boucle dès qu'un probleme survient
+		boolean arret = false;		
+		while(BF.containsAll(objectif) == false && arret == false){
+			System.out.println(BF);
+			//pour chaque règle, on va regarder si on peut l'effectuer
+			//si on peut on va alors ajouter le résultat à l'ensemble des faits
+			for(int i = 0; i < regles.size(); i++){
+				//on va regarder chaque proposition de la prémisse et on va regarder si on l'a dans notre ensemble de faits
+				Regle regleCourante = regles.get(i);
+				//si l'ensemble des faits contient bien l'ensemble des propositions de la premisse
+				if(BF.containsAll(regleCourante.getPrem())){
+					for(int j = 0; j<regleCourante.getPrem().size(); j++){
+						String propCourante = regleCourante.getPrem().get(j);
+						//si l'ensemble des faits contient déjà la proposition, alors on ne l'ajoute pas
+						System.out.println(regleCourante.getRes());
+						if(BF.contains(regleCourante.getRes()) == false){
+							BF.add(regleCourante.getRes());
+						}
+					}
+					
+					//On supprime la règle car nous n'auront plus besoin de l'utiliser
+					//regles.remove(i);
+				}
+			}
+			
+		}	
 		
-		
-		
+		System.out.println(BF);
+
 	}
+
+
 }
--- src/Regle.java
+++ src/Regle.java
@@ -1,21 +1,40 @@
+import java.util.ArrayList;
+
 public class Regle {
 	//attributs
-	private String premisse;
+	private ArrayList<String> premisse;
 	private String resultat;
-	
+
 	Regle(String proposition){	
+		premisse = new ArrayList<String>();
 		//délimite la prémisse et le résultat sur la flèche
 		int delim = proposition.indexOf("-");
-		premisse = proposition.substring(0, delim);
 		resultat = proposition.substring(delim+2, proposition.length());
+		String partieGauche = proposition.substring(0, delim);
+
+		//gestion premisse
+		delim = partieGauche.indexOf("^");
+		while(partieGauche.length() != 1){
+			delim = partieGauche.indexOf("^");
+			premisse.add(partieGauche.substring(0, delim));
+			partieGauche = partieGauche.substring(delim+1, partieGauche.length());
+		}
+		premisse.add(partieGauche);
 	}
-	
-	public String getPrem(){
+
+
+	public void display(){
+		for(int i = 0; i < premisse.size(); i++)
+			System.out.print(premisse.get(i)+" ");
+		System.out.print("\n");
+	}
+
+	public ArrayList<String> getPrem(){
 		return premisse;
 	}
-	
+
 	public String getRes(){
 		return resultat;
 	}
-	
+
 }
