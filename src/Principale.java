1e0fbb21ede643953ff1ac47099286c86cca66ec
Alexandre <lefol.alexandre@gmail.com>
2015-10-23 13:06:35 +0200

    résolution conflit

--- src/Principale.java
+++ src/Principale.java
@@ -15,14 +15,11 @@ public class Principale {
 
 
 		//objectif : {nuage, Z, T, R}
-<<<<<<< HEAD
-		objectif.add("pluie");
+		/*objectif.add("pluie");
 		objectif.add("nuage");
-=======
-		/*objectif.add("tagada");
+		bjectif.add("tagada");
 		objectif.add("soleil");
 		objectif.add("neige");*/
->>>>>>> 3a9e239023d041c90457db3e50a0bbb95a6fda8b
 		/*for(Regle regle : regles){
 			System.out.println(regle.getPrem());
 		}*/
