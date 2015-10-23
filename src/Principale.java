d900fd1f76777e089b37d7ac03b9b984f87195b0
Alexandre <lefol.alexandre@gmail.com>
2015-10-23 13:17:38 +0200

    test

--- src/Principale.java
+++ src/Principale.java
@@ -1,43 +1,7 @@
-import java.util.ArrayList;
 
 public class Principale {
-
-	public static void main(String[] args) {
+	public static void main(String args[]){
 		
-		
-		MyFrame fenetre = new MyFrame("Système expert");
-	    
-		
-		
-		
-		
-		
-
-
-		//objectif : {nuage, Z, T, R}
-		/*objectif.add("pluie");
-		objectif.add("nuage");
-		bjectif.add("tagada");
-		objectif.add("soleil");
-		objectif.add("neige");*/
-		/*for(Regle regle : regles){
-			System.out.println(regle.getPrem());
-		}*/
-		//ChainageAvant chainageAvant = new ChainageAvant(regles, BF, objectif);
-		/*ChainageArriere chainageArriere= new ChainageArriere(regles, BF, objectif);
-		BF=chainageArriere.run();
-		System.out.println(BF);*/
-		
-		/*if(chainageArriere.test("fraise tagada")==true){
-			System.out.println("verifie");
-		}*/
-		
-		
-		//BF = chainageAvant.run();
-		/*System.out.println(BF);
-		ArrayList<String> objectifsRunProfondeur = chainageAvant.runProfondeur();
-		System.out.println("objectifs trouvés : "+objectifsRunProfondeur);*/
 	}
-
 
 }
